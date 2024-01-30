e28fd8018ea1f47dc44542197fe66a394a315e6f
markzzw <453646474@qq.com>
2024-01-30 01:18:08 +0000

    feat: controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hello/:name')
+  getHello(@Param('name') a: string): string {
+    return `Say hello to ${a}`;
+  }
+
+  @Post('create')
+  createHello(@Body() body: { name: string; age: string }) {
+    return `Create hello to ${body.name}, age is ${body.age}`;
+  }
+
+  @Put('edit/:name')
+  changeHelloAge(
+    @Param('name') name: string,
+    @Body()
+    body: {
+      age?: string;
+    },
+  ) {
+    return `Change ${name}'s age to ${body.age}`;
+  }
+
+  @Delete('delete/:name')
+  deleteHello(@Param('name') name: string) {
+    return `Delete ${name}`;
   }
 }
